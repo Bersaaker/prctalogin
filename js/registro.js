342f18879c222e072df7601d1c052378c98cfaf9
Alejandro Avendaño <alex.aven.luna@gmail.com>
2022-10-14 08:56:30 -0500

    video 8

--- js/registro.js
+++ js/registro.js
@@ -16,6 +16,7 @@ function registro(e){
     let passVal = pass.value;
 
     if(nombreVal==''||emailVal==''||userVal==''||passVal==''){
+        creaMensaje('Verifica tus campos', 'danger');
         return;
     }
     const usuario = {
@@ -31,5 +32,16 @@ function registro(e){
         user.value = '';
         pass.value = '';
 
-    console.log('usuario guardado');
+    creaMensaje('Usuario Registrado!', 'success');
+}
+
+function creaMensaje(texto, tipo){
+    const nuevoElemento = document.createElement('div');
+    nuevoElemento.innerText = texto;
+    nuevoElemento.classList.add('alert', 'alert-' + tipo);
+    const divMensaje = document.getElementById('mensaje');
+    divMensaje.appendChild(nuevoElemento);
+    setTimeout(function(){
+        nuevoElemento.remove();
+    }, 2000);
 }
